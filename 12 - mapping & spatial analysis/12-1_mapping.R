729794deb66e2a863ec28602569217eb16826ef1
devanmcg <devan.mcgranahan@gmail.com>
2019-04-22 11:58:47 -0500

    update mapping files

--- 12 - mapping & spatial analysis/12-1_mapping.R
+++ 12 - mapping & spatial analysis/12-1_mapping.R
@@ -1,54 +1,42 @@
 install.packages(pacman)
-pacman::p_load(maps, ggplot2, ggmap, broom, rgdal, rgeos,plyr, maptools )
+pacman::p_load(maps, plyr, tidyverse, ggmap, broom, rgdal, rgeos, maptools, sf, viridis )
+
+source('https://raw.githubusercontent.com/devanmcg/rangeR/master/R/CustomGGplotThemes.R')
+
+# Load the three .Rdata files
+    # nd.features.d
+    # fear.us
+    # us.epa.III
 
 # Get started with maps
-  library(maps)
-  map() 
-  map('usa')
-  map('state')
-  map('county')
-  map('state', add=TRUE, lwd=2)
+  # maps package gives us maps for base graphics 
+  # Note that map() function conflicts with purrr package from tidyverse
+  maps::map() 
+  par(mar=c(4, 2, 4, 2))
+  maps::map('usa')
+  maps::map('state')
+  maps::map('county')
+  maps::map('state', add=TRUE, lwd=2)
   
-# Additional features in ggmap
-  # Fetch coordinates of places by name
-  library(ggmap)
-  fargo <- geocode("Fargo, North Dakota", messsaging=FALSE) 
-  # Add to map
-  points(fargo, pch=21, bg="blue", col="white", cex=2)
+  # Add points to map
+  points(subset(nd.features.d, name=="Fargo")[4:5],
+         pch=21, bg="blue", col="white", cex=2)
   
   # zoom in
-  map('county', 'north dakota', lwd=1, col="grey60")
-  map('state', add=TRUE, lwd=2)
-  points(fargo, pch=21, bg="blue", col="white", cex=2)
+  par(mar=c(4, 2, 4, 2))
+  maps::map('county', 'north dakota', lwd=1, col="grey60")
+  maps::map('state', add=TRUE, lwd=2)
+  points(subset(nd.features.d, name=="Fargo")[4:5],
+          pch=21, bg="blue", col="white", cex=3)
   
-  nd.features <- rbind( data.frame(feature="city", name="Fargo", 
-                                 geocode("Fargo, North Dakota")), 
-                      data.frame(feature="city", name="Grand Forks", 
-                                 geocode("Grand Forks, North Dakota")), 
-                     data.frame(feature="city", name="Bismarck", 
-                                geocode("Bismarck, North Dakota")),
-                      data.frame(feature="city", name="Minot", 
-                                 geocode("Minot, North Dakota")), 
-                     data.frame(feature="rec", name="Hettinger", 
-                                geocode("Hettinger, North Dakota")), 
-                     data.frame(feature="rec", name="Williston", 
-                                geocode("Williston, North Dakota")), 
-                     data.frame(feature="rec", name="Carrington", 
-                                geocode("Carrington, North Dakota")), 
-                     data.frame(feature="rec", name="Dickinson", 
-                                geocode("Dickinson, North Dakota")),
-                     data.frame(feature="rec", name="Central Grasslands", 
-                                geocode("Streeter, North Dakota")), 
-                     data.frame(feature="rec", name="Langdon", 
-                                geocode("Langdon, North Dakota")))
-  
-  with(subset(nd.features, feature=="city"), 
-       points(lon, lat, pch=21, bg="blue", col="white", cex=2))
-  with(subset(nd.features, feature=="city"), 
-       text(lon, lat, pos=3, labels=name, col="blue", cex=1))
-  with(subset(nd.features, feature=="rec"), 
+  # Add more points:
+  with(subset(nd.features.d, feature=="city"),
+       points(lon, lat, pch=21, bg="blue", col="white", cex=2) )
+  with(subset(nd.features.d, feature=="city"), 
+       text(lon, lat, pos=3, labels=name, col="blue", cex=1) )
+  with(subset(nd.features.d, feature=="rec"), 
        points(lon, lat, pch=24, bg="orange", col="white", cex=2))
-  with(subset(nd.features, feature=="rec"), 
+  with(subset(nd.features.d, feature=="rec"), 
        text(lon, lat, pos=3, labels=name, col="black", cex=1))
   
 # Better mapping with ggplot
@@ -58,19 +46,30 @@ pacman::p_load(maps, ggplot2, ggmap, broom, rgdal, rgeos,plyr, maptools )
   
   # Plot map with ggplot
     # note use of geom_polygon
-    library(ggplot2)
-    L48.gg <- ggplot() +
+    (L48.gg <- ggplot() +
           geom_polygon(data=l48, aes(x=long, y=lat, group=group), 
-                       color="white", fill="grey90", size=0.25)
-    L48.gg 
+                       color="white", fill="grey80", size=0.25) )
     L48.gg + theme_bw() 
-    L48.gg + theme_nothing() 
-    (L48.gg <- L48.gg  + theme_bw() + coord_map() ) 
-# Add spatial point data via geom_point:
-L48.gg + geom_point(data=nd.features, aes(x=lon, y=lat, shape=feature, color=feature))
+    L48.gg + theme_nothing() # from ggmap
+    L48.gg + theme_map()     # from rangeR
     
+    (L48.gg1 <- L48.gg  + theme_bw() + 
+                  coord_map() ) # Manage projections with a coord_
+    L48.gg1 + coord_map("stereographic")
+    L48.gg1 + coord_map("polyconic")
+    L48.gg1 + coord_map("conic", 30)
+    L48.gg1 + coord_map("conic", 90)
+    L48.gg1 + coord_map("guyou")
+    
+    (L48.gg <- L48.gg  + theme_map() + 
+                coord_map("polyconic")) 
+
+# Add spatial point data via geom_point:
+  L48.gg +  geom_point(data=nd.features.d, 
+                       aes(x=lon, y=lat, 
+                           shape=feature, color=feature))
 # Import spatial points data 
-  str(fear.us)
+  class(fear.us)
   # Extract, combine relevant data for mapping
   fear.us@data$id <- rownames(fear.us@data)
   fear.us.pts <- data.frame(objectid=fear.us$OBJECTID, 
@@ -81,25 +80,56 @@ L48.gg + geom_point(data=nd.features, aes(x=lon, y=lat, shape=feature, color=fea
   L48.gg + geom_point(data=fear.us.pts, aes(x=long, y=lat)) 
   
   # Crop map to regional area of interest:
-  L48.gg + geom_point(data=fear.us.pts, aes(x=long, y=lat)) +
-    coord_fixed(1.3, xlim=c(-104, max(l48$long)), ylim=l48$lat)
+  L48.gg + geom_point(data=fear.us.pts, 
+                      aes(x=long, y=lat)) +
+          coord_fixed(ratio = 1.3, 
+                      xlim=c(-104, max(l48$long)), 
+                      ylim=l48$lat)
   
-# Import shapefile
-  str(us.epa.III)
-  # Messy!
-  # And coordinate systems are off
-  # spTransform converts to long/lat
+# ggplot a shapefile
+  class(us.epa.III)
+  # First, make sure Coordinate Reference Systems match up 
+  proj4string(fear.us)  # Long Lat WGS84
+  proj4string(us.epa.III) # Aaea = Albers Equal Area
+
+  # CRS differ 
+  # spTransform converts CRS of any Spatial* to long/lat
   epa.regions <- us.epa.III
   epa.regions  <- spTransform(us.epa.III, CRS("+init=epsg:4326"))
   epa.regions@data$id <- rownames(epa.regions@data)
-  epa.regions.points <- tidy(epa.regions, region="id")
-  epa.regions.df <- join(epa.regions.points, epa.regions@data, by="id")
+  epa.regions.points <- broom::tidy(epa.regions, region="id")
+  epa.regions.df <- plyr::join(epa.regions.points, epa.regions@data, by="id")
   
   # Map species distribution on top of ecoregions
   L48.gg + geom_polygon(data=epa.regions.df[epa.regions.df$NA_L1CODE %in% 
                                               c('8','9'),], 
                         aes(x=long, y=lat, group=group, 
-                            alpha=0.5, fill=NA_L1NAME)) +
-    scale_alpha_continuous(guide=FALSE) + 
+                            fill=NA_L1NAME), alpha=0.5) +
     geom_point(data=fear.us.pts, aes(x=long, y=lat)) +
     coord_fixed(1.3, xlim=c(-104, max(l48$long)), ylim=l48$lat)
+  
+#
+# Package sf is a 'tidy" solution for handling spatial data
+#
+  # use st_read to access your original shapefile 
+  # st_as_sf works on Spatial* objects already in R
+  
+  st_as_sf(us.epa.III ) %>%
+  ggplot( ) + theme_bw() + 
+    geom_sf(aes(fill = NA_L1NAME), alpha=0.5) +
+              scale_fill_viridis("Level 1 ecoregion", 
+                                 discrete=TRUE, direction =-1)
+  
+  # Convert data to sf objects
+  epa_sf <- st_as_sf(us.epa.III )
+  fear_sf <- st_as_sf(fear.us ) %>%
+                st_crop(xmin=-104, xmax=-67, 
+                        ymin=25.13, ymax=49.38 ) 
+
+  ggplot() + theme_nothing() + 
+    geom_sf(data=filter(epa_sf, NA_L1CODE %in%  c('8','9')), 
+            aes(fill=NA_L1NAME), alpha=0.5 ) + 
+    geom_sf(data=fear_sf, pch=21, bg="white", 
+            col="black", stroke=1.5) + 
+    scale_fill_viridis("Level 1 ecoregion", 
+                       discrete=TRUE, direction =-1)
--- 12 - mapping & spatial analysis/13-2_spatial_interpolation.R
+++ 12 - mapping & spatial analysis/13-2_spatial_interpolation.R
@@ -1,4 +1,5 @@
-install.packages(c("ggmap", "plyr", "akima"))
+install.packages(c("pacman"))
+pacman::p_load(ggplot2, ggmap, plyr, lubridate, akima)
 
 ndawn.d <- read.csv("./ndawn_fall_2016.csv")
 
@@ -16,78 +17,105 @@ head(ndawn.d)
   nd.counties <- subset(map_data("county"), region =="north dakota")
 
 # Combine stations with map
-  (gg1 + geom_polygon(data=nd.counties, aes(x=long, y=lat, group=group), 
+  (gg1 + geom_polygon(data=nd.counties, 
+                      aes(x=long, y=lat, group=group), 
                color="grey60", fill=NA, size=0.75) + 
-      geom_polygon(data=nd, aes(x=long, y=lat, group=group), 
-               color="black", fill=NA, size=0.75) )
+        geom_path(data=nd, 
+                     aes(x=long, y=lat, group=group), 
+                 color="black", size=0.75) )
 
   library(plyr)
-  count(ndawn.d, vars=c("Year", "Month", "Day"))
+  plyr::count(ndawn.d, vars=c("Year", "Month", "Day"))
   
-  last.week <- subset(ndawn.d, Month==11 & Day==14)
+   one.week <- subset(ndawn.d, Month==11 & Day==14)
 
   (gg2 <- ggplot()  + coord_map() + theme_bw() + 
     geom_polygon(data=nd.counties, aes(x=long, y=lat, group=group), 
                  color="grey60", fill=NA, size=0.75) + 
     geom_polygon(data=nd, aes(x=long, y=lat, group=group), 
                  color="black", fill=NA, size=0.75) +
-    geom_point(data=last.week, aes(x=Longitude, y=Latitude, 
-                                   colour=Max.Temp), size=4) )
+    geom_point(data=one.week, aes(x=Longitude, y=Latitude, 
+                                   colour=MaxTemp), size=4) )
     gg2 + scale_colour_gradient(low = "blue", high = "red")  
                               
 # Interpolate
   library(akima)
-  interpTemp <- with(last.week, interp(x=Longitude, y=Latitude, 
-                                        z=Max.Temp))
+  interpTemp <- with(one.week, interp(x=Longitude, y=Latitude, 
+                                        z=MaxTemp))
   interpTemp.df <- as.data.frame(interp2xyz(interpTemp))
-  names(interpTemp.df) <- c("long", "lat", "max.temp")
+  names(interpTemp.df) <- c("long", "lat", "MaxTemp")
 
 # Map interpolated temperature
   (gg3 <- ggplot()  + coord_map() + theme_bw() +
           geom_tile(data=interpTemp.df, aes(x=long, y=lat, 
-                                      fill=max.temp)) ) 
+                                      fill=MaxTemp)) ) 
   gg3 + scale_fill_gradient(low = "blue", high = "red") 
   
-  gg3 + geom_polygon(data=nd.counties, aes(x=long, y=lat, group=group), 
-                         color="grey60", fill=NA, size=0.75) + 
+  gg3 + geom_path(data=nd.counties, 
+                  aes(x=long, y=lat, group=group), 
+                     color="grey60", size=0.75) + 
     geom_polygon(data=nd, aes(x=long, y=lat, group=group), 
                  color="black", fill=NA, size=0.75) +
-    geom_point(data=last.week, aes(x=Longitude, y=Latitude), size=3)  +
+    geom_point(data=one.week, aes(x=Longitude, y=Latitude), size=3)  +
     scale_fill_gradient(low = "blue", high = "red") 
   
 # Look at trends over time 
-   fall.monthly <- ddply(ndawn.d, .(Station.Name,Latitude, Longitude, Month), 
-                       summarize, max=max(Max.Temp))
+  # Summarize max temps by month
+  ndawn.d<- ndawn.d %>%
+    mutate(Month = lubridate::month(Month, 
+                                    label=TRUE, 
+                                    abbr=FALSE))
+  
+   fall.monthly <- ddply(ndawn.d, .(StationName,Latitude, 
+                                    Longitude, Month), 
+                        summarize, 
+                        max=max(MaxTemp)) 
+                  
    
-# Semi-automatic interpolation:
-    interpolator <- function(x) {
-        interp <- with(x, interp(x=Longitude, y=Latitude, 
-                                            z=max))
-       interp.df <- as.data.frame(interp2xyz(interp))
-       names(interp.df) <- c("long", "lat", "max.temp")
-       return(interp.df) }
-    
-   monthly.interps <- rbind(data.frame(month="August", 
-                     interpolator(subset(fall.monthly, Month==8) )), 
-          data.frame(month="September", 
-                      interpolator(subset(fall.monthly, Month==9) )), 
-          data.frame(month="October", 
-                     interpolator(subset(fall.monthly, Month==10) )),
-          data.frame(month="November", 
-                     interpolator(subset(fall.monthly, Month==11) )) ) 
-    
-
+  # Get interpolations for each month w/ custom function (see for loop alternative below)
+    monthly.interps <- ddply(fall.monthly, 
+                              .(Month), 
+                             function(x) {
+                               interp <- with(x, interp(x=Longitude, 
+                                                        y=Latitude, 
+                                                        z=max))
+                               interp.df <- as.data.frame(interp2xyz(interp))
+                               names(interp.df) <- c("long", "lat", "MaxTemp")
+                               return(interp.df) } ) 
+  # Plot
     ggplot()  + coord_map() + theme_bw() +
-      geom_tile(data=monthly.interps, aes(x=long, y=lat, 
-                                        fill=max.temp)) +
-      geom_polygon(data=nd.counties, aes(x=long, y=lat, group=group), 
-                   color="grey60", fill=NA, size=0.75) + 
-      geom_polygon(data=nd, aes(x=long, y=lat, group=group), 
-               color="black", fill=NA, size=0.75) +
+      geom_tile(data=monthly.interps, 
+                aes(x=long, y=lat, fill=MaxTemp)) +
+      geom_path(data=nd.counties, 
+                aes(x=long, y=lat, group=group), 
+                color="grey60", size=0.75) + 
+      geom_path(data=nd, 
+                aes(x=long, y=lat, group=group), 
+               color="black", size=0.75) +
       geom_point(data=ndawn.d, 
                  aes(x=Longitude, y=Latitude), size=3)  +
       scale_fill_gradient(low = "blue", high = "red", 
-                          na.value="transparent") +
-         facet_wrap(~month)
-
-
+                          na.value="transparent", 
+                          name="Monthly Max Temp") +
+      theme(legend.position = "top") +
+      facet_wrap(~Month)
+    
+    
+# An alternative to ddply using a for loop
+    # Note vectorized solutions like plyr or dplyr functions 
+    # are considered better for programming in R, 
+    # but sometimes for loops are more intuitive to write & easier to operate 
+    
+    monthly.interps <- data.frame() 
+    
+    for(i in length(unique(fall.monthly$Month))) {
+      i = 1 
+      M = unique(fall.monthly$Month)[1]
+      d = subset(fall.monthly, Month == M)
+      interp <- with(d, interp(x=Longitude, 
+                               y=Latitude, 
+                               z=max))
+      mon.int <- as.data.frame(interp2xyz(interp))
+      names(mon.int) <- c("long", "lat", "MaxTemp")
+      monthly.interps <- rbind(monthly.interps, mon.int)
+    }
